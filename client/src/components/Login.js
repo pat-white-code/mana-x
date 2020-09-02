9a67d1e69ac2a372e2d51394c6e30787799af369
pat-white-code <pat.k.white@gmail.com>
2020-09-01 22:13:47 -0500

    text fields and mockup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,27 @@
 import React from "react";
 import styles from "./Login.module.css";
 
+const classes = {
+  icon: {
+    padding: '10px',
+    minWidth: '40px',
+    position: 'absolute'
+  }
+}
+
 const Login = () => {
   return (
     <div className={styles["background"]}>
       <div className={styles["main-container"]}>
-        
+        <div className={styles['header']}>
+          <h1>LOGIN</h1>
+        </div>
+        <div>
+          <form>
+            <i className={'fa fa-user'} styles={classes.icon}></i>
+            <input type='text' className={styles['input-field']}></input>
+          </form>
+        </div>
       </div>
     </div>
   );
